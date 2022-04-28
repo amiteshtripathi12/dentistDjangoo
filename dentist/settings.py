bc08e8bcae4773b269218152f06bc85339bc9169
manihritik886.com <manihritik886@gmail.com>
2022-04-28 18:46:43 +0530

    tweaked app for heroku

--- dentist/settings.py
+++ dentist/settings.py
@@ -115,9 +115,10 @@ STATICFILES_DIRS = (
      os.path.join(BASE_DIR,'static'),
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 
