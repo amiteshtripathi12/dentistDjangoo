875286f391bf6fd94dd4d065b4195c5353f0db84
manihritik886.com <manihritik886@gmail.com>
2022-04-29 14:02:37 +0530

    tweaked settings for heroku

--- dentist/settings.py
+++ dentist/settings.py
@@ -69,13 +69,30 @@ WSGI_APPLICATION = 'dentist.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DB2',
+        'USER': 'postgres',
+        'PASSWORD': 'Htcdesire@1',
+        'HOST': 'localhost',
+        'PORT':'5432'
+    }
+}
+
+
+
+
+
+
+'''
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 '''
+'''
 #dj_from_env = dj_database_url.config(conn_max_age = 600)
-
 DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
