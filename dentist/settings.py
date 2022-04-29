4a526f845c9153e3528084ffa9b13629fd6a524b
manihritik886.com <manihritik886@gmail.com>
2022-04-29 11:29:29 +0530

    The firsttime

--- dentist/settings.py
+++ dentist/settings.py
@@ -73,12 +73,13 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+#dj_from_env = dj_database_url.config(conn_max_age = 600)
 
-dj_from_env = dj_database_url.config(conn_max_age = 600)
 DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
-
+'''
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
